e8f255bdf96db7fdb2e269a798cdc3638f727dca
Surapunya Thongdee <marchsrp@gmail.com>
2021-11-11 03:34:53 +0700

    Initial Commit

---
+++ Pantip-Clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pantip-Clone
+//
+//  Created by Surapunya Thongdee on 11/11/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pantip-Clone/Pantip_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pantip_CloneApp.swift
+//  Pantip-Clone
+//
+//  Created by Surapunya Thongdee on 11/11/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Pantip_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
